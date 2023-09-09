229deff76876eb5cec8a067a03a7688d0ec84358
espocia <joshuajonellegarbin.espocia@gmail.com>
2023-09-09 19:33:44 +0800

    env test

--- src/client.js
+++ src/client.js
@@ -1,11 +1,14 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-// const id = import.meta.env.VITE_SANITY_PROJECT_ID;
-// const token = import.meta.env.VITE_SANITY_TOKEN;
+const id_test = import.meta.env.VITE_SANITY_PROJECT_ID;
+const token_test = import.meta.env.VITE_SANITY_TOKEN;
+
+console.log(id_test, token_test);
 
 const id = "sefyg5lv";
-const token = "sktsFyfvx14MfAQHLrlKIakCnGFrvq9sv9vg6Otijm90vg4cA3KA6UthzyorJnSuXRYSruOfSofCO9u4KOOFwg44jYAe0cmMU3MEkeTTGX9pVUHP57ma45ukMZhyGPuZzwGmq0Fz5prq5Jpjsmw0b4zkVXhWM6t0VNJpGNj6EboZLj9dsZ5d"
+const token =
+  "sktsFyfvx14MfAQHLrlKIakCnGFrvq9sv9vg6Otijm90vg4cA3KA6UthzyorJnSuXRYSruOfSofCO9u4KOOFwg44jYAe0cmMU3MEkeTTGX9pVUHP57ma45ukMZhyGPuZzwGmq0Fz5prq5Jpjsmw0b4zkVXhWM6t0VNJpGNj6EboZLj9dsZ5d";
 
 export const client = createClient({
   projectId: id,
