55e481fff6c594c873f8c971e7e190046d480cf1
espocia <joshuajonellegarbin.espocia@gmail.com>
2023-09-09 19:25:24 +0800

    env test

--- src/client.js
+++ src/client.js
@@ -1,8 +1,11 @@
 import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-const id = import.meta.env.VITE_SANITY_PROJECT_ID;
-const token = import.meta.env.VITE_SANITY_TOKEN;
+// const id = import.meta.env.VITE_SANITY_PROJECT_ID;
+// const token = import.meta.env.VITE_SANITY_TOKEN;
+
+const id = "sefyg5lv";
+const token = "sktsFyfvx14MfAQHLrlKIakCnGFrvq9sv9vg6Otijm90vg4cA3KA6UthzyorJnSuXRYSruOfSofCO9u4KOOFwg44jYAe0cmMU3MEkeTTGX9pVUHP57ma45ukMZhyGPuZzwGmq0Fz5prq5Jpjsmw0b4zkVXhWM6t0VNJpGNj6EboZLj9dsZ5d"
 
 export const client = createClient({
   projectId: id,
